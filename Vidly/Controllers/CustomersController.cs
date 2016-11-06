629b1b94432f450bb8612b47cfff0949bf6d4dca
Cezary Banasik <cezary.banasik@sage.com>
2016-11-06 22:13:29 +0100

    After validation

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CustomersController()
+        {
+            this.context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = new Customer(),
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
+            if (customer.Id == 0)
+            {
+                context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = this.context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int id)
+        {
+            var customer = this.context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var customer = context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        // POST: Customers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        ////private IEnumerable<Customer> GetCustomers()
+        ////{
+        ////    return new Customer[]
+        ////    {
+        ////        new Customer { Id = 1, Name = "John Smith" },
+        ////        new Customer { Id = 2, Name = "Mary Williams" }
+        ////    };
+        ////}
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public MoviesController()
+        {
+            this.context = new ApplicationDbContext();
+        }
+
+        public ActionResult New()
+        {
+            var genres = context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        // POST: Movies/Save
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = this.context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var movie = context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new MovieFormViewModel(movie)
+            {
+                Genres = context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int id)
+        {
+            var movie = context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" },
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
